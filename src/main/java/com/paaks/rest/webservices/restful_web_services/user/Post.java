e84794b7a0339201ec265c88c0808734aad8654d
Paa Kwasi Kodua <paakwasi.kodua@turntabl.io>
2024-08-19 15:34:23 +0000

    create Post entity

---
+++ src/main/java/com/paaks/rest/webservices/restful_web_services/user/Post.java
@@ -0,0 +1,25 @@
+package com.paaks.rest.webservices.restful_web_services.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private User user;
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,11 @@ insert into user_details(id, name, birth_date) values(10001, 'Paaks', current_da
 insert into user_details(id, name, birth_date) values(10002, 'Seventeen', current_date());
 
 insert into user_details(id, name, birth_date) values(10003, 'BTS', current_date());
+
+insert into post(id, description, user_id) values(20001, 'I want to play tennis', 10001);
+
+insert into post(id, description, user_id) values(20002, 'I want to learn French', 10002);
+
+insert into post(id, description, user_id) values(20003, 'I want to learn Spanish', 10003);
+
+insert into post(id, description, user_id) values(20004, 'Seventeen is amazing!!!', 10001);
