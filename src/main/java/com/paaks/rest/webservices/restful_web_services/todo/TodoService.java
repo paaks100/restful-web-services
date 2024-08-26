ad79a906a160ac465a35e427fdb7936f69cabeeb
Paa Kwasi Kodua <paakwasi.kodua@turntabl.io>
2024-08-26 11:12:39 +0000

    implemented TodoResource

--- src/main/java/com/paaks/rest/webservices/restful_web_services/todo/TodoService.java
+++ src/main/java/com/paaks/rest/webservices/restful_web_services/todo/TodoService.java
@@ -15,9 +15,9 @@ public class TodoService {
     private static int todosCount = 0;
 
     static {
-        todos.add(new Todo(++todosCount, "paaks", "Get AWS Certified 1", LocalDate.now().plusYears(1), false));
-        todos.add(new Todo(++todosCount, "paaks", "Learn DevOps 1", LocalDate.now().plusYears(2), false));
-        todos.add(new Todo(++todosCount, "paaks", "Learn Full Stack Development 1", LocalDate.now().plusYears(3), false));
+        todos.add(new Todo(++todosCount, "paaks", "Get AWS Certified", LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(++todosCount, "paaks", "Learn DevOps", LocalDate.now().plusYears(2), false));
+        todos.add(new Todo(++todosCount, "paaks", "Learn Full Stack Development", LocalDate.now().plusYears(3), false));
     }
 
     public List<Todo> findByUsername(String username) {
