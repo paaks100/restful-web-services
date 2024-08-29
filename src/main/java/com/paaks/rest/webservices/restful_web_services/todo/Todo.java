7be0d99c89dd18383e8804e4162e636ad1754d01
Paa Kwasi Kodua <paakwasi.kodua@turntabl.io>
2024-08-29 13:31:07 +0000

    changed id from int to Integer

--- src/main/java/com/paaks/rest/webservices/restful_web_services/todo/Todo.java
+++ src/main/java/com/paaks/rest/webservices/restful_web_services/todo/Todo.java
@@ -12,7 +12,7 @@ public class Todo {
 
     @Id
     @GeneratedValue
-    private int id;
+    private Integer id;
     private String username;
 
     @Size(min = 10, message = "Enter at least 10 characters")
@@ -22,7 +22,7 @@ public class Todo {
 
     public Todo() {}
 
-    public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
+    public Todo(Integer id, String username, String description, LocalDate targetDate, boolean done) {
         this.id = id;
         this.username = username;
         this.description = description;
@@ -30,11 +30,11 @@ public class Todo {
         this.done = done;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
